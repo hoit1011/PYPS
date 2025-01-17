792122922e5eed7c32759b486d9d2c37b456e8ca
hoit1011 <parksm071011@gmail.com>
2025-01-17 17:12:26 +0900

    11725_S2

---
+++ 11725_S2.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+adj =[[] for _ in range(N)]
+for i in range(N-1):
+    u,v = map(int,input().split())
+    adj[u-1].append(v-1)
+    adj[v-1].append(u-1)
+visit = [False] * N
+arr = [-1] * N
+
+def dfs(st):
+    visit[st] = True
+    for i in adj[st]:
+        if not visit[i]:
+            arr[i] = st
+            dfs(i)
+
+dfs(0)
+
+for i in range(1,N):
+    print(arr[i]+1,end=" ")
