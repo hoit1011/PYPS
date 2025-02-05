cca2b0e0c25d5058f7da9b6b8c1df904e3e8f50a
hoit1011 <parksm071011@gmail.com>
2025-02-05 21:31:45 +0900

    15651_S3

---
+++ 15651_S3.py
@@ -0,0 +1,12 @@
+def dfs():
+    if len(arr) == M:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,N+1):
+        arr.append(i)
+        dfs()
+        arr.pop()
+
+N , M = map(int, input().split())
+arr = []
+dfs()
