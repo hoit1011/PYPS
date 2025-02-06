7b71cd751c1ee645b1c491c116c1c38e2aff174e
hoit1011 <parksm071011@gmail.com>
2025-02-06 19:19:08 +0900

    2178_S1.py

--- 2178_S1.py
+++ 2178_S1.py
@@ -1,7 +1,33 @@
+from collections import deque
+
 N, M = map(int,input().split())
 arr = []
 for _ in range(N):
-    s = input()
-    arr.append(s)
+    arr.append(input())
 
-print(arr)
+visit = [[False] * M for _ in range(N)]
+dist = [[False] * M for _ in range(N)]
+queue = deque([(0,0)])
+visit[0][0] = True
+dist[0][0] = 0
+
+while len(queue) != 0:
+    r,c = queue.popleft()
+    if r > 0 and arr[r-1][c] == '1' and not visit[r-1][c]:
+        queue.append((r-1,c))
+        visit[r-1][c] = True
+        dist[r-1][c] = dist[r][c] + 1
+    if r < N-1 and arr[r+1][c] == '1' and not visit[r+1][c]:
+        queue.append((r+1,c))
+        visit[r+1][c] = True
+        dist[r+1][c] = dist[r][c] + 1
+    if c < M - 1 and arr[r][c+1] == '1' and not visit[r][c+1]:
+        queue.append((r,c+1))
+        visit[r][c+1] = True
+        dist[r][c+1] = dist[r][c] + 1
+    if c > 0 and arr[r][c-1] == '1' and not visit[r][c-1]:
+        queue.append((r,c-1))
+        visit[r][c-1] = True
+        dist[r][c-1] = dist[r][c] + 1
+
+print(dist[N-1][M-1] + 1)
