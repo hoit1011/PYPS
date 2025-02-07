b173a0800e12b81bf935b85a86cd9de0bb35c78d
hoit1011 <parksm071011@gmail.com>
2025-02-07 20:27:53 +0900

    28702_B1

---
+++ 28702_B1.py
@@ -0,0 +1,37 @@
+A = input()
+B = input()
+C = input()
+
+if(A.isdigit()):
+    n = int(A)
+    n += 3
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
+elif(B.isdigit()):
+    n = int(B)
+    n += 2
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
+elif(C.isdigit()):
+    n = int(C)
+    n += 1
+    if n % 3 == 0 and n % 5 == 0:
+        print("FizzBuzz")
+    elif n % 3 == 0:
+        print("Fizz")
+    elif n % 5 == 0:
+        print("Buzz")
+    else:
+        print(n)
