7afa24b0ac48f1b808b20b29bf4f12fbbcf6ad77
hoit1011 <parksm071011@gmail.com>
2025-02-15 12:06:01 +0900

    14888_S1

--- 14888_S1.py
+++ 14888_S1.py
@@ -1,41 +1,29 @@
-from itertools import permutations
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 li = list(map(int,input().split()))
-a,s,d,f = map(int,input().split())
-arr = []
+op = list(map(int, input().split()))
 
-for _ in range(a):
-    arr.append(1)
-for _ in range(s):
-    arr.append(2)
-for _ in range(d):
-    arr.append(3)
-for _ in range(f):
-    arr.append(4)
+max_res = - int(1e9)
+min_res = int(1e9)
 
-Max = -1e9
-Min = 1e9
-for permutation in permutations(arr,N-1):
-    cnt = 0
+def dfs(depth, total, plus, minus, multiply, divide):
+    global max_res, min_res
+    if depth == N:
+        max_res = max(max_res,total)
+        min_res = min(min_res,total)
+        return
+    if plus:
+        dfs(depth + 1,total+li[depth],plus-1,minus,multiply,divide)
+    if minus:
+        dfs(depth + 1,total-li[depth],plus,minus-1,multiply,divide)
+    if multiply:
+        dfs(depth + 1,total*li[depth],plus,minus,multiply-1,divide)
+    if divide:
+        dfs(depth + 1, int(total / li[depth]), plus, minus, multiply, divide - 1)
 
-    for i in range(N):
-        if i == 0:
-            cnt += li[i]
-            continue
+dfs(1,li[0],op[0],op[1],op[2],op[3])
 
-        if(permutation[i-1] == 1):
-            cnt += li[i]
-        if(permutation[i-1] == 2):
-            cnt -= li[i]
-        if(permutation[i-1] == 3):
-            cnt *= li[i]
-        if(permutation[i-1] == 4):
-            cnt = int(cnt / li[i])
-        if(permutation == (2,4,1,1,3)):
-            print(cnt)
-    if Max < cnt:
-        print(permutation)
-    Max = max(Max, cnt)
-    Min = min(Min,cnt)
-
-print(Max,Min)
+print(max_res)
+print(min_res)
