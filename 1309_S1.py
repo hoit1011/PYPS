2b310409887871b8b1ccdae446f4de5921fba687
hoit1011 <parksm071011@gmail.com>
2025-03-21 15:23:47 +0900

    1309_S1

---
+++ 1309_S1.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * N 
+
+
+if N == 1:
+    print("3")
+else:
+    dp[0] = 3
+    dp[1] = 7
+    for i in range(2,N):
+        dp[i] = (2*dp[i-1] + dp[i-2]) % 9901
+    print(dp[N-1])
