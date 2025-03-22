459043364fdfa33d311d6ca1b127e9225f9f0c11
hoit1011 <parksm071011@gmail.com>
2025-03-22 20:52:59 +0900

    11053_S2

--- 11053_S2.py
+++ 11053_S2.py
@@ -1,12 +1,10 @@
 N = int(input())
-numbers = list(map(int,input().split()))
-arr = []
+li = list(map(int,input().split()))
+
+dp = [1] * N
+
 for i in range(N):
-    s = numbers[i]
-    cnt = 0
-    for j in range(i,N):
-        if s < numbers[j]:
-            cnt += 1
-            s = numbers[j]
-    arr.append(cnt)
-print(max(arr)+1)
+    for j in range(i):
+        if li[i] > li[j]:
+            dp[i] = max(dp[i],dp[j] + 1)
+print(max(dp))
