8606ca7e32ec802bd2c1372395741ff38de0c2f7
hoit1011 <parksm071011@gmail.com>
2025-03-31 14:09:31 +0900

    20920_S3

--- 20920_S3.py
+++ 20920_S3.py
@@ -2,9 +2,13 @@ N, M = map(int,input().split())
 
 d = {}
 for i in range(N):
-    S = input()
+    S = input().strip()
     if len(S) >= M:
         if S not in d:
             d[S] = 0
         d[S] += 1
 
+arr = sorted(d.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+
+for i in arr:
+    print(i[0])
