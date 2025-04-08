10697747b9f20dd43429a0855cc91493cc23a5c9
hoit1011 <parksm071011@gmail.com>
2025-04-08 15:51:35 +0900

    2293_G4

---
+++ 2293_G4.py
@@ -0,0 +1,13 @@
+N, K = map(int,input().split())
+arr = []
+for i in range(N):
+    arr.append(int(input()))
+
+dp = [0 for _ in range(K + 1)]
+dp[0] = 1
+
+for i in arr:
+    for j in range(i,K+1):
+        dp[j] += dp[j-i]
+
+print(dp[K])
